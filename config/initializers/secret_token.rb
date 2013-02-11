800e956483c39d9c2464d10ca0ffe5b58431a572
SnS-Phors <sns-phors@Notebook-PC.(none)>
2013-02-11 17:28:22 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '50de4507aa716e88655cd7f28436bb0957c33c93227dfaa7d58a9e85923106a5ca72e8204f84b99d8f8965370bb85139f6d49dbc151a0b45890f3727ee9484d3'
